8ae5daf9811efeb7db2af4354f62d1a22d4258fd
Pedro Gomes <37028775+pedro-gomes-92@users.noreply.github.com>
2024-10-25 13:16:10 -0700

    feat: disable some features

--- rush-plugins/rush-migrate-subspace-plugin/src/migrateProject.ts
+++ rush-plugins/rush-migrate-subspace-plugin/src/migrateProject.ts
@@ -1,55 +1,74 @@
-import {
-  chooseCreateOrSelectSubspacePrompt,
-  chooseSubspacePrompt,
-  createSubspacePrompt
-} from './prompts/subspace';
+import { chooseSubspacePrompt } from './prompts/subspace';
 import { addProjectToSubspace } from './functions/addProjectToSubspace';
-import { initSubspaces } from './functions/initSubspaces';
 import { chooseProjectPrompt, confirmNextProjectPrompt } from './prompts/project';
-import { chooseRepositoryPrompt } from './prompts/repository';
 import Console from './providers/console';
 import { getRootPath } from './utilities/path';
 import chalk from 'chalk';
-import { createSubspace } from './functions/createSubspace';
 import { updateSubspace } from './functions/updateSubspace';
 import { getRushSubspaceConfigurationFolderPath, isSubspaceSupported } from './utilities/subspace';
 import { IRushConfigurationProjectJson } from '@rushstack/rush-sdk/lib/api/RushConfigurationProject';
 import { queryProjects } from './utilities/project';
-import { isExternalMonorepo, querySubspaces } from './utilities/repository';
+import {
+  getRushSubspacesConfigurationJsonPath,
+  isExternalMonorepo,
+  querySubspaces
+} from './utilities/repository';
+import { RushConstants } from '@rushstack/rush-sdk';
 
 export async function migrateProject(): Promise<void> {
   Console.debug('Executing project migration command...');
 
   Console.title(`🔍 Analyzing if monorepo ${chalk.underline(getRootPath())} supports subspaces...`);
 
+  /**
+   * WARN: Disabling auto subspace initialization for now.
+   * if (!isSubspaceSupported()) {
+   *     Console.warn(
+   *  `The monorepo ${chalk.bold(getRootPath())} doesn't contain subspaces. Initializing subspaces...`
+   *);
+   * await initSubspaces();
+   * }
+   */
+
   const targetSubspaces: string[] = querySubspaces();
-  if (!isSubspaceSupported()) {
-    // Start subspaces
-    Console.warn(
-      `The monorepo ${chalk.bold(getRootPath())} doesn't contain subspaces. Initializing subspaces...`
+  if (!isSubspaceSupported() || targetSubspaces.length === 0) {
+    Console.error(
+      `The monorepo ${chalk.bold(getRootPath())} doesn't support subspaces! Make sure you have ${chalk.bold(
+        getRushSubspacesConfigurationJsonPath()
+      )} with the ${chalk.bold(RushConstants.defaultSubspaceName)} subspace. Exiting...`
     );
-    await initSubspaces();
+    return;
   }
 
   Console.success(`Monorepo ${chalk.bold(getRootPath())} fully supports subspaces!`);
 
   Console.title(`🔍 Finding projects to migrate to ${chalk.bold(getRootPath())}...`);
-  const sourceMonorepoPath: string = await chooseRepositoryPrompt();
 
-  Console.warn(
-    `The script will migrate from ${chalk.bold(sourceMonorepoPath)} to ${chalk.bold(getRootPath())}`
-  );
+  /**
+   * WARN: Disabling different repository selection for now.
+   * const sourceMonorepoPath: string = await chooseRepositoryPrompt();
+   * Console.warn(
+   *   `The script will migrate from ${chalk.bold(sourceMonorepoPath)} to ${chalk.bold(getRootPath())}`
+   * );
+   */
 
-  const subspaceSelectionType: string = await chooseCreateOrSelectSubspacePrompt(targetSubspaces);
+  const sourceMonorepoPath: string = getRootPath();
 
-  const targetSubspace: string =
-    subspaceSelectionType === 'new'
-      ? await createSubspacePrompt(targetSubspaces)
-      : await chooseSubspacePrompt(targetSubspaces);
+  /**
+   * WARN: Disabling creating new subspaces for now.
+   *   const subspaceSelectionType: string = await chooseCreateOrSelectSubspacePrompt(targetSubspaces);
+   *
+   * const targetSubspace: string =
+   *  subspaceSelectionType === 'new'
+   *    ? await createSubspacePrompt(targetSubspaces)
+   *    : await chooseSubspacePrompt(targetSubspaces);
+   *
+   * if (!targetSubspaces.includes(targetSubspace)) {
+   *  await createSubspace(targetSubspace);
+   *}
+   */
 
-  if (!targetSubspaces.includes(targetSubspace)) {
-    await createSubspace(targetSubspace);
-  }
+  const targetSubspace: string = await chooseSubspacePrompt(targetSubspaces);
 
   // Loop until user asks to quit
   do {
@@ -59,7 +78,7 @@ export async function migrateProject(): Promise<void> {
       : sourceProjects.filter(({ subspaceName }) => subspaceName !== targetSubspace);
 
     if (sourceAvailableProjects.length === 0) {
-      Console.success(
+      Console.error(
         `No available projects found in the monorepo ${chalk.bold(sourceMonorepoPath)}! Exiting...`
       );
       return;
